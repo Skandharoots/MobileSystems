19ca5c623cc00261d453754d3e3488965fccc002
Marek Kopania <m.k.kopania@gmail.com>
2023-10-07 01:42:39 +0200

    Added a new view

--- Lab/app/src/main/java/com/example/lab_application/MainActivity.kt
+++ Lab/app/src/main/java/com/example/lab_application/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.lab_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceActivity.Header
+import android.view.View
 import com.example.lab_application.databinding.ActivityMainBinding
-import java.text.NumberFormat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,27 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.calculateButton.setOnClickListener{calculateTip()}
+        binding.switchViewButton.setOnClickListener{ switchView() }
     }
 
-    private fun calculateTip() {
-        val stringInTextField = binding.costOfServiceEditText.text.toString()
-        val cost = stringInTextField.toDoubleOrNull()
-        if (cost == null) {
-            binding.tipResult.text = ""
-            return
-        }
-        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-            R.id.option_twenty_percent -> 0.20
-            R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
-        }
-        var tip = tipPercentage * cost
-        if (binding.roundUpSwitch.isChecked) {
-            tip = kotlin.math.ceil(tip)
-        }
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-
+    private fun switchView() {
+        val intent = Intent(this, TipCalculatorSite::class.java)
+        startActivity(intent)
     }
+
 }

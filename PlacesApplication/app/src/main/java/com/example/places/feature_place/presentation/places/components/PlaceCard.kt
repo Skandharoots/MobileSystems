1c88fb460ce6d31ed3cba96af89e4b16a3b4cfff
Marek Kopania <m.k.kopania@gmail.com>
2023-10-15 12:11:47 +0200

    Added PlaceCard view model

--- PlacesApplication/app/src/main/java/com/example/places/feature_place/presentation/places/components/PlaceCard.kt
+++ PlacesApplication/app/src/main/java/com/example/places/feature_place/presentation/places/components/PlaceCard.kt
@@ -3,52 +3,111 @@ package com.example.places.feature_place.presentation.places.components
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScopeInstance.align
+import androidx.compose.foundation.layout.BoxScopeInstance.matchParentSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScopeInstance.align
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.InspectableModifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.places.R
+import com.example.places.feature_place.domain.model.Place
 import java.util.Date
 
 @Composable
 fun PlaceCard(
-    city: String,
-    date: Date,
-    about: String,
-    rating: Int,
-    picture: ImageView,
-    checked: Boolean,
-    onCheck: () -> Unit,
-    modifier: Modifier = Modifier
+    place: Place,
+    modifier: Modifier = Modifier,
+    cornerRadius: Dp = 10.dp,
+    onDeleteClick: () -> Unit
 ) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically
+    Box(
+      modifier = modifier
     ) {
-//        RadioButton(selected = checked,
-//                    onClick = onCheck,
-//                    colors = RadioButtonDefaults.colors(
-//                        selectedColor = MaterialTheme.colorScheme.primary,
-//                        unselectedColor = MaterialTheme.colorScheme.onBackground
-//                    )
-//        )
-//        Spacer(modifier = Modifier.width(8.dp))
-//        Text(text = city, style = MaterialTheme.typography.headlineMedium)
-//        //Text(text = date, )
-//        ConstraintLayout {
-//            Icon(painter = , contentDescription = )
-//            Text()
-//        }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val img = painterResource(id = androidx.appcompat.R.drawable.abc_ic_menu_share_mtrl_alpha)
+
+            Image(
+                img,
+                alignment = Alignment.Center,
+                contentDescription = "",
+                contentScale = ContentScale.Fit,
+                modifier = modifier
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(end = 34.dp)
+        ) {
+            Text(
+                text = place.city,
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.onSurface,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = place.date.toString(),
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurface,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = place.about,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurface,
+                maxLines = 10,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+        IconButton(
+            onClick = onDeleteClick,
+            modifier = Modifier.align(Alignment.BottomEnd)) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete place"
+            )
+        }
     }
 }

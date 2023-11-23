ab49a954207095c9e2cdfc34b964e6e65154306f
Marek Kopania <m.k.kopania@gmail.com>
2023-11-23 15:15:57 +0100

    update: added google maps marker

--- Lab/app/src/main/java/com/example/lab_application/fragment/MapFragment.kt
+++ Lab/app/src/main/java/com/example/lab_application/fragment/MapFragment.kt
@@ -3,12 +3,17 @@ package com.example.lab_application.fragment
 import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.lab_application.R
 import com.example.lab_application.databinding.FragmentListBinding
@@ -30,25 +35,27 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     private var myMap: GoogleMap? = null
 
-    private final val FIN_PERM_Code = 1
+    private final val FIN_PERM_CODE = 101
 
     private var currentLocation : Location? = null
 
-    private var fusedLocationProviderClient : FusedLocationProviderClient? = null
+    private lateinit var fusedLocationProviderClient : FusedLocationProviderClient
+    private var locationPermissionGranted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val view = binding.root
-        var supportMapFragment : SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        supportMapFragment.getMapAsync(this)
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        getCurrentLocationUser()
         return view
     }
 
@@ -64,13 +71,47 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         myMap = googleMap
-        val sydney = LatLng(-34.0, 151.0)
-        var options = MarkerOptions().position(sydney).title("Sydney")
-        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-        myMap?.addMarker(options)
-        myMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val latLng = currentLocation?.let { LatLng(it.latitude, it.longitude) }
+        var options = latLng?.let { MarkerOptions().position(it).title("Your location") }
+        options?.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+        latLng?.let { CameraUpdateFactory.newLatLng(it) }?.let { myMap?.animateCamera(it) }
+        latLng?.let { CameraUpdateFactory.newLatLngZoom(it, 7f) }?.let { myMap?.animateCamera(it) }
+        if (options != null) {
+            myMap?.addMarker(options)
+        }
     }
 
-
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun getCurrentLocationUser() {
+            val locationPermissionRequest = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) { permissions ->
+                when {
+                    permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                        Toast.makeText(requireContext(), "Location permission granted", Toast.LENGTH_LONG).show()
+                    }
+                    permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                        Toast.makeText(requireContext(), "Approximate location permission granted", Toast.LENGTH_LONG).show()
+                    } else -> {
+                    Toast.makeText(requireContext(), "Location permission rejected", Toast.LENGTH_LONG).show()
+                }
+                }
+            }
+            locationPermissionRequest.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION))
+        if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED) {
+            val getLocation =
+                fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                    if (location != null) {
+                        currentLocation = location
+                        var supportMapFragment : SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+                        supportMapFragment.getMapAsync(this)
+                    }
+                }
+        }
+    }
 
 }

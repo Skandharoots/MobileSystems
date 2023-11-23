6729d26981d801a1ba5dbe01f9e0a9b01d75fa30
Marek Kopania <m.k.kopania@gmail.com>
2023-11-23 14:29:56 +0100

    update: added google maps marker

--- Lab/app/src/main/java/com/example/lab_application/fragment/MapFragment.kt
+++ Lab/app/src/main/java/com/example/lab_application/fragment/MapFragment.kt
@@ -1,23 +1,43 @@
 package com.example.lab_application.fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.findNavController
 import com.example.lab_application.R
 import com.example.lab_application.databinding.FragmentListBinding
 import com.example.lab_application.databinding.FragmentMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-class MapFragment : Fragment() {
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
+
+class MapFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var myMap: GoogleMap
+    private var myMap: GoogleMap? = null
+
+    private final val FIN_PERM_Code = 1
+
+    private var currentLocation : Location? = null
+
+    private var fusedLocationProviderClient : FusedLocationProviderClient? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -26,8 +46,9 @@ class MapFragment : Fragment() {
     ): View? {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val view = binding.root
-
-
+        var supportMapFragment : SupportMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        supportMapFragment.getMapAsync(this)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
         return view
     }
 
@@ -38,5 +59,18 @@ class MapFragment : Fragment() {
             findNavController().navigate(R.id.action_mapFragment_to_listFragment)
         }
         binding.myToolbar.setTitle("Map")
+
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        myMap = googleMap
+        val sydney = LatLng(-34.0, 151.0)
+        var options = MarkerOptions().position(sydney).title("Sydney")
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+        myMap?.addMarker(options)
+        myMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+
+
 }
